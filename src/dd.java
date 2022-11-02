b1b53d77a0820616cabb5221f845494d7a275101
hsy <hsy.com>
2022-11-02 15:51:57 +0800

    hello

---
+++ src/dd.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: hsy
+ * @Date: 2022/11/02/15:47
+ * @Description:
+ */
+public class dd {
+    public static void main(String[] args) {
+        System.out.println("aa");
+        System.out.println("bb");
+    }
+}
